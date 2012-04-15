675165b7339d8d2af87afc193569f7aaa538fded
rknLA <kevin@rkn.la>
2012-04-14 21:40:25 -0700

    Display total queue for debugging purposes.

--- public/javascripts/presenter.js
+++ public/javascripts/presenter.js
@@ -1,4 +1,3 @@
-var fire = new Firebase('http://demo.firebase.com/sharedcinema');
 var cinema = new Firebase('http://demo.firebase.com/sharedcinema/kevin_demo/cinema');
 var queue = new Firebase('http://demo.firebase.com/sharedcinema/kevin_demo/cinema/queue');
 
@@ -9,41 +8,38 @@ $(document).ready(function () {
 
   debug.log('Debug Console launched.');
 
-  var queueQuery = queue.limit(10);
+  var queueQuery = queue.limit(3);
   //create the queue
   queueQuery.on('value', function(snapshot) {
     var q = snapshot.val();
     console.log("this is the queue:");
     console.log(q);
 //    $('#rankedQueue').
+    
   });
 
   $('#submit-new-video').click(function() {
-    var title = $('#new-video-title').val();
-    var youtube = $('#new-video-id').val();
+    var titleEl = $('#new-video-title');
+    var youtubeEl = $('#new-video-id');
 
-    queueVideo(title, youtube);
+    queueVideo(titleEl.val(), youtubeEl.val());
 
+    titleEl.val('');
+    youtubeEl.val('');
   });
 
+  $('.upvote').click(function(e) {
+    console.log(this);
+  });
 
-});
-
-
-fire.on('value', function(snap) {
-  console.log(snap);
-  console.log(snap.exportVal());
 });
 
 
 cinema.on('value', function(snapshot) {
-  var c = snapshot.val();
-  console.log("cinema snapshot:");
-  console.log(c)
-  console.log(snapshot.exportVal());
+  var c = snapshot.exportVal();
   debug.log("cinema updated:");
   debug.log(snapshot.exportVal());
-
+  printWholeQueue(c.queue);
 });
 
 function queueVideo(title, youtube_id) {
@@ -52,6 +48,23 @@ function queueVideo(title, youtube_id) {
       queueItem.setPriority(1);
     }
   });
+}
+
+function upvoteVideo(item_id, current_priority) {
+  var itemRef = new Firebase('http://demo.firebase.com/sharedcinema/kevin_demo/cinema/queue/' + item_id);
+
+  itemRef.setPriority(current_priority + 1);
+}
+
+function printWholeQueue(queue) {
+  var queueHandle = $('#completeQueue');
+  for (key in queue) {
+    var item = queue[key];
+    var html = '<li><a href="#" class="upvote" data-index="' + item.key + '" data-votes="' + item['.priority'] + '" >+1</a>&nbsp;&nbsp;';
+    html += '(' + item['.priority'] +')&nbsp;&nbsp;';
+    html += '<a href="http://www.youtube.com/watch?v=' + item.youtube_id + '">' + item.title + '</a> </li>';
+    queueHandle.append(html);
+  }
 }
 
 
